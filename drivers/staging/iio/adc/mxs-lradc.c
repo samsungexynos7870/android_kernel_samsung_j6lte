1c23a7e1d4eb2db743726125f9f44e15effc5258
Kristina Martšenko <kristina.martsenko@gmail.com>
2015-01-25 18:28:19 +0200

    iio: mxs-lradc: separate touchscreen and buffer virtual channels
    The touchscreen was initially designed [1] to map all of its physical
    channels to one virtual channel, leaving buffered capture to use the
    remaining 7 virtual channels. When the touchscreen was reimplemented
    [2], it was made to use four virtual channels, which overlap and
    conflict with the channels the buffer uses.
    
    As a result, when the buffer is enabled, the touchscreen's virtual
    channels are remapped to whichever physical channels the buffer was
    configured with, causing the touchscreen to read those instead of the
    touch measurement channels. Effectively the touchscreen stops working.
    
    So here we separate the channels again, giving the touchscreen 2 virtual
    channels and the buffer 6. We can't give the touchscreen just 1 channel
    as before, as the current pressure calculation requires 2 channels to be
    read at the same time.
    
    This makes the touchscreen continue to work during buffered capture. It
    has been tested on i.MX28, but not on i.MX23.
    
    [1] 06ddd353f5c8 ("iio: mxs: Implement support for touchscreen")
    [2] dee05308f602 ("Staging/iio/adc/touchscreen/MXS: add interrupt driven
    touch detection")
    
    Signed-off-by: Kristina Martšenko <kristina.martsenko@gmail.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Cc: Stable@vger.kernel.org
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/mxs-lradc.c
+++ drivers/staging/iio/adc/mxs-lradc.c
@@ -1151,10 +1151,9 @@ static irqreturn_t mxs_lradc_handle_irq(int irq, void *data)
 				LRADC_CTRL1_LRADC_IRQ(TOUCHSCREEN_VCHANNEL2));
 	}
 
-	if (iio_buffer_enabled(iio)) {
-		if (reg & lradc->buffer_vchans)
-			iio_trigger_poll(iio->trig);
-	} else if (reg & LRADC_CTRL1_LRADC_IRQ(0)) {
+	if (iio_buffer_enabled(iio))
+		iio_trigger_poll(iio->trig);
+	else if (reg & LRADC_CTRL1_LRADC_IRQ(0))
 		complete(&lradc->completion);
 	}
 
