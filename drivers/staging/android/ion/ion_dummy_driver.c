3e97cdef766bcf6c5f7d4bf5ffd11ab9ed99533e
John Stultz <john.stultz@linaro.org>
2019-02-13 12:44:18 -0800

    ANDROID: ion_dummy_driver: Rework ion_dummy_driver to avoid direct indexing into the heaps
    The dummy_heaps structure currently is setup so that each
    entry's id value, matches its index in the array. This is
    convenient in some spots, but limits how we can change things.
    
    This patch reworks the logic so that we avoid directly indexing
    into dummy_heaps using an id value. Instead we iterate over the
    list and then compare the type.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: If1083da5b8a403101c23946a3bbfbe53063fccb8

--- drivers/staging/android/ion/ion_dummy_driver.c
+++ drivers/staging/android/ion/ion_dummy_driver.c
@@ -104,6 +104,8 @@ static int __init ion_dummy_init(void)
 	return 0;
 err:
 	for (i = 0; i < dummy_ion_pdata.nr; i++) {
+		struct ion_platform_heap *heap_data = &dummy_ion_pdata.heaps[i];
+
 		if (!IS_ERR_OR_NULL(heaps[i]))
 			ion_heap_destroy(heaps[i]);
 
@@ -134,10 +136,7 @@ static void __exit ion_dummy_exit(void)
 	ion_device_destroy(idev);
 
 	for (i = 0; i < dummy_ion_pdata.nr; i++) {
-		if (!IS_ERR_OR_NULL(heaps[i]))
-			ion_heap_destroy(heaps[i]);
-	}
-	kfree(heaps);
+		struct ion_platform_heap *heap_data = &dummy_ion_pdata.heaps[i];
 
 		if (!IS_ERR_OR_NULL(heaps[i]))
 			ion_heap_destroy(heaps[i]);
